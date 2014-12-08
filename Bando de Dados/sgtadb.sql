eafc2f99b0571257fbb8a2ec4ce615a1df05c8b4
Filipe Mei <peticormei@gmail.com>
2014-12-08 09:54:43 -0300

    Modificação no Banco de Dados

--- Bando de Dados/sgtadb.sql
+++ Bando de Dados/sgtadb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 08-Dez-2014 às 13:30
+-- Generation Time: 08-Dez-2014 às 13:52
 -- Versão do servidor: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -17,6 +17,443 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
+-- Database: `cdcol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cds`
+--
+
+CREATE TABLE IF NOT EXISTS `cds` (
+  `titel` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `interpret` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `jahr` int(11) DEFAULT NULL,
+`id` bigint(20) unsigned NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Extraindo dados da tabela `cds`
+--
+
+INSERT INTO `cds` (`titel`, `interpret`, `jahr`, `id`) VALUES
+('Beauty', 'Ryuichi Sakamoto', 1990, 1),
+('Goodbye Country (Hello Nightclub)', 'Groove Armada', 2001, 4),
+('Glee', 'Bran Van 3000', 1997, 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cds`
+--
+ALTER TABLE `cds`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cds`
+--
+ALTER TABLE `cds`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;--
+-- Database: `phpmyadmin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_bookmark`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_bookmark` (
+`id` int(11) NOT NULL,
+  `dbase` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `label` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `query` text COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Bookmarks';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_column_info`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_column_info` (
+`id` int(5) unsigned NOT NULL,
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `column_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `comment` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `mimetype` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `transformation` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `transformation_options` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT ''
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Column information for phpMyAdmin';
+
+--
+-- Extraindo dados da tabela `pma_column_info`
+--
+
+INSERT INTO `pma_column_info` (`id`, `db_name`, `table_name`, `column_name`, `comment`, `mimetype`, `transformation`, `transformation_options`) VALUES
+(25, 'sgtadb', 'aluno', 'estado', '', '', '_', ''),
+(24, 'sgtadb', 'aluno', 'cidade', '', '', '_', ''),
+(23, 'sgtadb', 'aluno', 'complemento', '', '', '_', ''),
+(22, 'sgtadb', 'aluno', 'numero', '', '', '_', ''),
+(21, 'sgtadb', 'aluno', 'endereco', '', '', '_', ''),
+(20, 'sgtadb', 'aluno', 'identidade', '', '', '_', ''),
+(19, 'sgtadb', 'aluno', 'cpf', '', '', '_', ''),
+(18, 'sgtadb', 'aluno', 'sexo', '', '', '_', ''),
+(17, 'sgtadb', 'aluno', 'data', '', '', '_', ''),
+(16, 'sgtadb', 'aluno', 'nome', '', '', '_', ''),
+(15, 'sgtadb', 'aluno', 'id', '', '', '_', ''),
+(26, 'sgtadb', 'aluno', 'bairro', '', '', '_', ''),
+(27, 'sgtadb', 'aluno', 'telefone', '', '', '_', ''),
+(28, 'sgtadb', 'aluno', 'celular', '', '', '_', ''),
+(29, 'sgtadb', 'aluno', 'email', '', '', '_', ''),
+(30, 'sgtadb', 'aluno', 'observacao', '', '', '_', ''),
+(35, 'sgtadb', 'administrador', 'matricula', '', '', '_', ''),
+(36, 'sgtadb', 'administrador', 'id', '', '', '_', ''),
+(37, 'sgtadb', 'administrador', 'login', '', '', '_', ''),
+(38, 'sgtadb', 'administrador', 'senha', '', '', '_', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_designer_coords`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_designer_coords` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `v` tinyint(4) DEFAULT NULL,
+  `h` tinyint(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for Designer';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_history`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_history` (
+`id` bigint(20) unsigned NOT NULL,
+  `username` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `timevalue` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sqlquery` text COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='SQL history for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_navigationhiding`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_navigationhiding` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `item_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `item_type` varchar(64) COLLATE utf8_bin NOT NULL,
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Hidden items of navigation tree';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_pdf_pages`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_pdf_pages` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+`page_nr` int(10) unsigned NOT NULL,
+  `page_descr` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='PDF relation pages for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_recent`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_recent` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `tables` text COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Recently accessed tables';
+
+--
+-- Extraindo dados da tabela `pma_recent`
+--
+
+INSERT INTO `pma_recent` (`username`, `tables`) VALUES
+('root', '[{"db":"sgtadb","table":"aluno"},{"db":"sgtadb","table":"administrador"},{"db":"phpmyadmin","table":"pma_column_info"},{"db":"phpmyadmin","table":"pma_designer_coords"},{"db":"phpmyadmin","table":"pma_history"},{"db":"phpmyadmin","table":"pma_pdf_pages"},{"db":"phpmyadmin","table":"pma_recent"},{"db":"phpmyadmin","table":"pma_relation"},{"db":"cdcol","table":"cds"},{"db":"mysql","table":"user"}]');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_relation`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_relation` (
+  `master_db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `master_table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `master_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Relation table';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_savedsearches`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_savedsearches` (
+`id` int(5) unsigned NOT NULL,
+  `username` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `search_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `search_data` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Saved searches';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_table_coords`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_coords` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `pdf_page_number` int(11) NOT NULL DEFAULT '0',
+  `x` float unsigned NOT NULL DEFAULT '0',
+  `y` float unsigned NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for phpMyAdmin PDF output';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_table_info`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_info` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `display_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table information for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_table_uiprefs`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_uiprefs` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `prefs` text COLLATE utf8_bin NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Tables'' UI preferences';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_tracking`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_tracking` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `version` int(10) unsigned NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime NOT NULL,
+  `schema_snapshot` text COLLATE utf8_bin NOT NULL,
+  `schema_sql` text COLLATE utf8_bin,
+  `data_sql` longtext COLLATE utf8_bin,
+  `tracking` set('UPDATE','REPLACE','INSERT','DELETE','TRUNCATE','CREATE DATABASE','ALTER DATABASE','DROP DATABASE','CREATE TABLE','ALTER TABLE','RENAME TABLE','DROP TABLE','CREATE INDEX','DROP INDEX','CREATE VIEW','ALTER VIEW','DROP VIEW') COLLATE utf8_bin DEFAULT NULL,
+  `tracking_active` int(1) unsigned NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT COMMENT='Database changes tracking for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_userconfig`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_userconfig` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `timevalue` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `config_data` text COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='User preferences storage for phpMyAdmin';
+
+--
+-- Extraindo dados da tabela `pma_userconfig`
+--
+
+INSERT INTO `pma_userconfig` (`username`, `timevalue`, `config_data`) VALUES
+('root', '2014-12-08 12:36:44', '{"lang":"pt","collation_connection":"utf8mb4_general_ci","ThemeDefault":"pmahomme"}');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_usergroups`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_usergroups` (
+  `usergroup` varchar(64) COLLATE utf8_bin NOT NULL,
+  `tab` varchar(64) COLLATE utf8_bin NOT NULL,
+  `allowed` enum('Y','N') COLLATE utf8_bin NOT NULL DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='User groups with configured menu items';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_users`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_users` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `usergroup` varchar(64) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Users and their assignments to user groups';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pma_bookmark`
+--
+ALTER TABLE `pma_bookmark`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pma_column_info`
+--
+ALTER TABLE `pma_column_info`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `db_name` (`db_name`,`table_name`,`column_name`);
+
+--
+-- Indexes for table `pma_designer_coords`
+--
+ALTER TABLE `pma_designer_coords`
+ ADD PRIMARY KEY (`db_name`,`table_name`);
+
+--
+-- Indexes for table `pma_history`
+--
+ALTER TABLE `pma_history`
+ ADD PRIMARY KEY (`id`), ADD KEY `username` (`username`,`db`,`table`,`timevalue`);
+
+--
+-- Indexes for table `pma_navigationhiding`
+--
+ALTER TABLE `pma_navigationhiding`
+ ADD PRIMARY KEY (`username`,`item_name`,`item_type`,`db_name`,`table_name`);
+
+--
+-- Indexes for table `pma_pdf_pages`
+--
+ALTER TABLE `pma_pdf_pages`
+ ADD PRIMARY KEY (`page_nr`), ADD KEY `db_name` (`db_name`);
+
+--
+-- Indexes for table `pma_recent`
+--
+ALTER TABLE `pma_recent`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `pma_relation`
+--
+ALTER TABLE `pma_relation`
+ ADD PRIMARY KEY (`master_db`,`master_table`,`master_field`), ADD KEY `foreign_field` (`foreign_db`,`foreign_table`);
+
+--
+-- Indexes for table `pma_savedsearches`
+--
+ALTER TABLE `pma_savedsearches`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `u_savedsearches_username_dbname` (`username`,`db_name`,`search_name`);
+
+--
+-- Indexes for table `pma_table_coords`
+--
+ALTER TABLE `pma_table_coords`
+ ADD PRIMARY KEY (`db_name`,`table_name`,`pdf_page_number`);
+
+--
+-- Indexes for table `pma_table_info`
+--
+ALTER TABLE `pma_table_info`
+ ADD PRIMARY KEY (`db_name`,`table_name`);
+
+--
+-- Indexes for table `pma_table_uiprefs`
+--
+ALTER TABLE `pma_table_uiprefs`
+ ADD PRIMARY KEY (`username`,`db_name`,`table_name`);
+
+--
+-- Indexes for table `pma_tracking`
+--
+ALTER TABLE `pma_tracking`
+ ADD PRIMARY KEY (`db_name`,`table_name`,`version`);
+
+--
+-- Indexes for table `pma_userconfig`
+--
+ALTER TABLE `pma_userconfig`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `pma_usergroups`
+--
+ALTER TABLE `pma_usergroups`
+ ADD PRIMARY KEY (`usergroup`,`tab`,`allowed`);
+
+--
+-- Indexes for table `pma_users`
+--
+ALTER TABLE `pma_users`
+ ADD PRIMARY KEY (`username`,`usergroup`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pma_bookmark`
+--
+ALTER TABLE `pma_bookmark`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pma_column_info`
+--
+ALTER TABLE `pma_column_info`
+MODIFY `id` int(5) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=39;
+--
+-- AUTO_INCREMENT for table `pma_history`
+--
+ALTER TABLE `pma_history`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pma_pdf_pages`
+--
+ALTER TABLE `pma_pdf_pages`
+MODIFY `page_nr` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pma_savedsearches`
+--
+ALTER TABLE `pma_savedsearches`
+MODIFY `id` int(5) unsigned NOT NULL AUTO_INCREMENT;--
 -- Database: `sgtadb`
 --
 
@@ -101,7 +538,41 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `aluno`
 --
 ALTER TABLE `aluno`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;--
+-- Database: `test`
+--
+--
+-- Database: `webauth`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user_pwd`
+--
+
+CREATE TABLE IF NOT EXISTS `user_pwd` (
+  `name` char(30) COLLATE latin1_general_ci NOT NULL DEFAULT '',
+  `pass` char(32) COLLATE latin1_general_ci NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Extraindo dados da tabela `user_pwd`
+--
+
+INSERT INTO `user_pwd` (`name`, `pass`) VALUES
+('xampp', 'wampp');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_pwd`
+--
+ALTER TABLE `user_pwd`
+ ADD PRIMARY KEY (`name`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
