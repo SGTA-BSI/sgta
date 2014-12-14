087a3b25b1ca92157fb8203d6b0765fe5c4b67c7
Jorge Delgado <ajffdelgado@hotmail.com>
2014-12-14 18:54:27 -0300

    Banco de dados atualizado

--- Bando de Dados/sgtabd.sql
+++ Bando de Dados/sgtabd.sql
@@ -1,11 +1,11 @@
-﻿-- phpMyAdmin SQL Dump
--- version 4.2.11
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 14-Dez-2014 às 14:34
--- Versão do servidor: 5.6.21
--- PHP Version: 5.6.3
+-- Tempo de geração: 14/12/2014 às 22:50
+-- Versão do servidor: 5.6.16
+-- Versão do PHP: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,76 +17,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `sgtabd`
+-- Banco de dados: `sgtabd`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `admin`
---
-
-CREATE TABLE IF NOT EXISTS `admin` (
-`id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `data_nasc` varchar(250) NOT NULL,
-  `sexo` varchar(250) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `identidade` varchar(20) NOT NULL,
-  `endereco` varchar(50) NOT NULL,
-  `numero` varchar(11) NOT NULL,
-  `complemento` varchar(10) NOT NULL,
-  `cidade` varchar(25) NOT NULL,
-  `estado` varchar(250) NOT NULL,
-  `bairro` varchar(25) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `celular` varchar(20) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `observacao` varchar(250) NOT NULL,
-  `login` varchar(25) NOT NULL,
-  `senha` varchar(25) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `aluno`
---
-
-CREATE TABLE IF NOT EXISTS `aluno` (
-`id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `data_nasc` varchar(250) NOT NULL,
-  `sexo` varchar(250) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `identidade` varchar(20) NOT NULL,
-  `endereco` varchar(50) NOT NULL,
-  `numero` varchar(11) NOT NULL,
-  `complemento` varchar(10) NOT NULL,
-  `cidade` varchar(25) NOT NULL,
-  `estado` varchar(250) NOT NULL,
-  `bairro` varchar(25) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `celular` varchar(20) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `observacao` varchar(250) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `aluno`
---
-
-INSERT INTO `aluno` (`id`, `nome`, `data_nasc`, `sexo`, `cpf`, `identidade`, `endereco`, `numero`, `complemento`, `cidade`, `estado`, `bairro`, `telefone`, `celular`, `email`, `observacao`) VALUES
-(2, 'Antonio Jorge Ferreira Delgado Filho', '11/06/1993', 'Sexo', '101376344-04', '1000000', 'Rua São Benedito', '215', 'Casa A', 'Recife ', 'Pernambuco', 'Pina', '3216-9874', '9874-8587', 'bsi@bsi.com.br', 'Teste bem sucedido!!!');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `func`
+-- Estrutura para tabela `func`
 --
 
 CREATE TABLE IF NOT EXISTS `func` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(250) NOT NULL,
   `data_nasc` varchar(250) NOT NULL,
   `sexo` varchar(250) NOT NULL,
@@ -101,52 +42,22 @@ CREATE TABLE IF NOT EXISTS `func` (
   `telefone` varchar(20) NOT NULL,
   `celular` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `observacao` varchar(250) NOT NULL,
   `login` varchar(25) NOT NULL,
-  `senha` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `senha` varchar(25) NOT NULL,
+  `cargo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- Indexes for dumped tables
+-- Fazendo dump de dados para tabela `func`
 --
 
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
- ADD PRIMARY KEY (`id`);
+UPDATE `func` SET `id` = 1,`nome` = 'Teste 1',`data_nasc` = '11/11/1987',`sexo` = 'Masculino',`cpf` = '111.111.111-11',`identidade` = '11111',`endereco` = 'Rua Teste',`numero` = '111',`complemento` = '',`cidade` = 'Teste',`estado` = 'Teste',`bairro` = 'Teste',`telefone` = '(81)7888-7878',`celular` = '(78)7878-7878',`email` = 'teste@teste.com',`login` = '',`senha` = '',`cargo` = 'Professor' WHERE `func`.`id` = 1;
+UPDATE `func` SET `id` = 2,`nome` = 'Teste 2',`data_nasc` = '21/21/1999',`sexo` = 'Feminino',`cpf` = '   .   .   -  ',`identidade` = '14545',`endereco` = 'Teste',`numero` = '111',`complemento` = '',`cidade` = 'Teste',`estado` = 'Teste',`bairro` = 'Teste',`telefone` = '(  )    -    ',`celular` = '(  )    -    ',`email` = 'teste@teste.com',`login` = 'teste',`senha` = '123',`cargo` = 'Professor' WHERE `func`.`id` = 2;
+UPDATE `func` SET `id` = 3,`nome` = 'ad',`data_nasc` = '12/12/2012',`sexo` = 'Masculino',`cpf` = '111.111.111-12',`identidade` = '111',`endereco` = '1111',`numero` = '1111',`complemento` = '1111',`cidade` = '1111',`estado` = '111',`bairro` = '111',`telefone` = '(11)1111-1111',`celular` = '(77)7777-7777',`email` = 'teste@teste.com',`login` = 'teste',`senha` = '123',`cargo` = 'Atendente' WHERE `func`.`id` = 3;
+UPDATE `func` SET `id` = 4,`nome` = 'teste',`data_nasc` = '  /  /    ',`sexo` = 'Masculino',`cpf` = '222.222.222-22',`identidade` = '2222',`endereco` = '2222',`numero` = '222',`complemento` = '222',`cidade` = '222',`estado` = '2222',`bairro` = '222',`telefone` = '(22)2222-2222',`celular` = '(22)2222-2222',`email` = '222@222.com',`login` = 'teste',`senha` = '123',`cargo` = 'Professor' WHERE `func`.`id` = 4;
+UPDATE `func` SET `id` = 5,`nome` = 'Teste Final',`data_nasc` = '12/11/1121',`sexo` = 'Feminino',`cpf` = '111.111.111-33',`identidade` = '1111',`endereco` = 'teste final',`numero` = '333',`complemento` = '',`cidade` = 'final teste',`estado` = 'Final',`bairro` = 'Teste',`telefone` = '(21)5887-8787',`celular` = '(31)7878-7878',`email` = 'teste@teste.com',`login` = 'teste1',`senha` = '321',`cargo` = 'Atendente' WHERE `func`.`id` = 5;
 
---
--- Indexes for table `aluno`
---
-ALTER TABLE `aluno`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `cpf` (`cpf`,`identidade`,`email`);
-
---
--- Indexes for table `func`
---
-ALTER TABLE `func`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `aluno`
---
-ALTER TABLE `aluno`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `func`
---
-ALTER TABLE `func`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
