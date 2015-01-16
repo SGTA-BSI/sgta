a08a5a73e752134d673299f43c8b12cfead30753
Jorge Delgado <ajffdelgado@hotmail.com>
2015-01-16 13:39:21 -0300

    Criação Classe Medidas

---
+++ Código/sgta/src/com/sgta/usuario/dominio/Medidas.java
@@ -0,0 +1,107 @@
+package com.sgta.usuario.dominio;
+
+import java.util.Date;
+
+public class Medidas {
+
+	private Double bracos;
+	private Double peitoral;
+	private Double coxas;
+	private Double costas;
+	private Double panturrilha;
+	private Double trapezio;
+	private Double antebracos;
+	private Double cintura;
+	private Double peso;
+	private Double altura;
+	private Date data;
+
+	public Double getBracos() {
+		return bracos;
+	}
+
+	public void setBracos(Double bracos) {
+		this.bracos = bracos;
+	}
+
+	public Double getPeitoral() {
+		return peitoral;
+	}
+
+	public void setPeitoral(Double peitoral) {
+		this.peitoral = peitoral;
+	}
+
+	public Double getCoxas() {
+		return coxas;
+	}
+
+	public void setCoxas(Double coxas) {
+		this.coxas = coxas;
+	}
+
+	public Double getCostas() {
+		return costas;
+	}
+
+	public void setCostas(Double costas) {
+		this.costas = costas;
+	}
+
+	public Double getPanturrilha() {
+		return panturrilha;
+	}
+
+	public void setPanturrilha(Double panturrilha) {
+		this.panturrilha = panturrilha;
+	}
+
+	public Double getTrapezio() {
+		return trapezio;
+	}
+
+	public void setTrapezio(Double trapezio) {
+		this.trapezio = trapezio;
+	}
+
+	public Double getAntebracos() {
+		return antebracos;
+	}
+
+	public void setAntebracos(Double antebracos) {
+		this.antebracos = antebracos;
+	}
+
+	public Double getCintura() {
+		return cintura;
+	}
+
+	public void setCintura(Double cintura) {
+		this.cintura = cintura;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+}
