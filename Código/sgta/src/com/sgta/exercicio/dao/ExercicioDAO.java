dc1182367139cbf0ab96969097706fee55e7b70c
Jorge Delgado <ajffdelgado@hotmail.com>
2015-01-24 13:32:41 -0300

    Interface Criação de Treino

--- Código/sgta/src/com/sgta/exercicio/dao/ExercicioDAO.java
+++ Código/sgta/src/com/sgta/exercicio/dao/ExercicioDAO.java
@@ -2,10 +2,15 @@ package com.sgta.exercicio.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.sgta.bd.BD;
 import com.sgta.exercicio.dominio.Exercicio;
+import com.sgta.usuario.dominio.Pessoa;
+import com.sgta.usuario.dominio.Usuario;
 
 public class ExercicioDAO {
 	private static ExercicioDAO instancia = new ExercicioDAO();
@@ -31,6 +36,41 @@ public class ExercicioDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	public List<Exercicio> retornaTodosExercicios() {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		List<Exercicio> listaExercicios = new ArrayList<Exercicio>();
+
+		try {
+			connection = bd.getConnection();
+			statement = connection
+					.prepareStatement("SELECT * FROM exercicios");
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				Exercicio exercicio = new Exercicio();
+				exercicio.setId(resultSet.getInt("id"));
+				exercicio.setNome(resultSet.getString("nome"));
+				exercicio.setMusculo(resultSet.getString("musculo"));
+				listaExercicios.add(exercicio);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			bd.fecharConecaoMySQL();
+		}
+
+		return listaExercicios;
 	}
 
 }
